6cb8326e8f6246d18c649ce62c7f0077f430b0f4
superpower1 <superpiglet1@gmail.com>
2017-09-13 23:00:04 +1000

    test useFactory

--- injection/new-app/src/app/app.module.ts
+++ injection/new-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Product1Component } from './product1/product1.component';
 import { ProductService } from './shared/product.service';
+import { Product2Service } from './shared/product2.service';
 import { Product2Component } from './product2/product2.component';
 import { LogService } from './shared/log.service';
 
@@ -17,7 +18,22 @@ import { LogService } from './shared/log.service';
   imports: [
     BrowserModule
   ],
-  providers: [ProductService, LogService],
+  providers: [{
+    provide: ProductService,
+    useFactory: (printOut: LogService, appConfig) => {
+      if (appConfig.isDev) { 
+        return new ProductService;
+      } else {
+        return new Product2Service(printOut);
+      }
+    },
+    deps: [LogService, "APP_CONFIG"]
+  }, 
+  LogService,
+  {
+    provide: "APP_CONFIG", useValue: { isDev: true }
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- injection/new-app/src/app/product2/product2.component.ts
+++ injection/new-app/src/app/product2/product2.component.ts
@@ -5,10 +5,7 @@ import { Product2Service} from '../shared/product2.service';
 @Component({
   selector: 'app-product2',
   templateUrl: './product2.component.html',
-  styleUrls: ['./product2.component.css'],
-  providers: [{
-  	provide: ProductService, useClass: Product2Service
-  }]
+  styleUrls: ['./product2.component.css']
 })
 export class Product2Component implements OnInit {
 
