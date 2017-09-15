471ed4acd450f669fdda32bd86f7f98e1ede318c
superpower1 <superpiglet1@gmail.com>
2017-09-15 11:59:20 +1000

    add node test

---
+++ node/add.js
@@ -0,0 +1,5 @@
+var add = function (v1, v2) {
+	return v1+v2;
+}
+
+module.exports.add = add;
---
+++ node/print.js
@@ -0,0 +1,6 @@
+var add = require('./add.js').add;
+var fs = require('fs');
+
+console.log(add(1,2));
+
+console.log(fs);
