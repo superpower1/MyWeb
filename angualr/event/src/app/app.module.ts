0b4c01fddd401f627ddd62ecd7a785344118713c
superpower1 <superpiglet1@gmail.com>
2017-10-10 21:23:14 +1100

    add child component

--- angualr/event/src/app/app.module.ts
+++ angualr/event/src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BindComponent } from './bind/bind.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MultiplePipe } from './pipe/multiple.pipe';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindComponent
+    BindComponent,
+    MultiplePipe,
+    ChildComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,  
     FormsModule,
     ReactiveFormsModule
   ],
