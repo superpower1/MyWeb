9a37169dd5cf820e0f1947fa7c308f9ec02759a7
superpower1 <superpiglet1@gmail.com>
2017-11-01 22:37:10 +1100

    add some notes

--- ruby/ruby_standard.rb
+++ ruby/ruby_standard.rb
@@ -1,12 +1,12 @@
 代码规范
 
-1. 变量使用Snake case
+1. 变量使用小写，Snake case
   hello_world
 
 2. 常量使用大写
     HELLO_WORLD = 1
 
-3. 类与模块首字母大写，驼峰命名法
+3. 类与模块首字母大写，Camel case
     HelloWorld
 
 4. 不使用;
@@ -15,3 +15,5 @@
 case
 when
 end
+
+6. 字符串一般使用''，使用""可以插值"#{var}"，多行字符串可以使用<<-TEXT something TEXT (TEXT可以取任意名字)
