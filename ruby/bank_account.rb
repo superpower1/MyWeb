00bb48ff8f8585dec4155d933f4825c5190ebab7
superpower1 <superpiglet1@gmail.com>
2017-11-08 13:44:38 +1100

    improve bank account

--- ruby/bank_account.rb
+++ ruby/bank_account.rb
@@ -1,5 +1,7 @@
 class BankAccount
 
+  include Lib
+
   attr_reader :balance
 
   @@exchange_rate = 5
@@ -18,11 +20,47 @@ class BankAccount
   end
 
   def deposite(amount)
-    balance += amount
+    @balance += amount
   end
 
   def withdraw(amount)
     raise 'Overdraw!' if amount>@balance
-    balance -= amount
+    @balance -= amount
+  end
+end
+
+class CommBankAccount < BankAccount
+  def transfer(to_account, amount)
+    @balance -= amount
+    to_account.deposite(amount)
+  end
+
+  def withdraw(amount)
+    super
+    @balance -= 3
+  end
+end
+
+class ANZBankAccount < BankAccount
+
+  def withdraw(amount)
+    super
+    @balance -= 2
+  end
+end
+
+module Lib
+  BUCKETS = [0, 1000, 10_000, 50_000]
+  def annual_fee
+    case balance
+    when BUCKETS[0]...BUCKETS[1]
+      10
+    when BUCKETS[1]...BUCKETS[2]
+      5
+    when BUCKETS[2]...BUCKETS[3]
+      3
+    else
+      0
+    end
   end
 end
